237ba828acea997b55c1fe3c7ffa1aa478be6d68
Fancyee <74527909+kuma1105@users.noreply.github.com>
2022-06-29 17:48:41 +0900

    Make EditTodo Components

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,5 +1,7 @@
 import React, {Fragment, useState, useEffect} from "react";
 
+import EditTodo from "./EditTodo";
+
 const ListTodos = () => {
 
     const [todos, setTodos] = useState([])
@@ -46,7 +48,7 @@ const ListTodos = () => {
                 {todos.map(todo => (
                     <tr key={todo.todo_id}>
                         <td>{todo.description}</td>
-                        <td>Edit</td>
+                        <td><EditTodo todo={todo}/></td>
                         <td>
                             <button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button>
                         </td>
