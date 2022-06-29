d355de9ae4d6743e824353b9c58b3489ec608499
Fancyee <74527909+kuma1105@users.noreply.github.com>
2022-06-29 17:02:15 +0900

    Make ListTodos Components

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, {Fragment, useState, useEffect} from "react";
 
 const ListTodos = () => {
-    return <h1>List Todos</h1>;
+
+    const [todos, setTodos] = useState([])
+
+    // delete function
+    const deleteTodo = async id => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            })
+
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+        } catch (e) {
+            console.error(e.message)
+        }
+    }
+
+    const getTodos = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/todos")
+            const jsonData = await response.json()
+
+            setTodos(jsonData);
+        } catch (e) {
+            console.error(e.message);
+        }
+    }
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    return (
+        <Fragment>
+            <table className="table mt-5 text-center">
+                <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+                </thead>
+                <tbody>
+                {todos.map(todo => (
+                    <tr key={todo.todo_id}>
+                        <td>{todo.description}</td>
+                        <td>Edit</td>
+                        <td>
+                            <button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </Fragment>);
 }
 
 export default ListTodos;
